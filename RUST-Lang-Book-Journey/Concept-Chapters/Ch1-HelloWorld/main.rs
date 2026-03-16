641265e64eb44fe835683c4367cfc1260be95188
komalkoutilya <komalkoutilya@gmail.com>
2026-03-16 23:31:20 +0530

    created a basic cargo project

--- RUST-Lang-Book-Journey/Concept-Chapters/Ch1-HelloWorld/main.rs
+++ RUST-Lang-Book-Journey/Concept-Chapters/Ch1-HelloWorld/main.rs
@@ -1,3 +1,7 @@
+// fn indicates the code block is a function
+// main acts as an entry point of the code
 fn main() {
+    // here the println is a normal function
+    // but the println! is a macro (so it has exclamatory mark after function name
     println!("Hello, world!");
 }
